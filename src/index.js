23e49b4b1bfaf504d03e6bc48f63733dd3d32b5a
harol <harold@halopowered.com>
2019-11-28 14:23:43 +0200

    asdsad

--- src/index.js
+++ src/index.js
@@ -2,9 +2,31 @@ import './styles.css';
 import './views/main-view.js';
 import { Router } from '@vaadin/router';
 
+let swRegistration;
+
 window.addEventListener('load', () => {
   initRouter();
+  initNotifications();
 });
+
+function initNotifications() {
+  if ('serviceWorker' in navigator && 'PushManager' in window) {
+    console.log('Service Worker and Push is supported');
+
+    navigator.serviceWorker.register('sw.js')
+        .then(function(swReg) {
+          console.log('Service Worker is registered', swReg);
+
+          swRegistration = swReg;
+        })
+        .catch(function(error) {
+          console.error('Service Worker Error', error);
+        });
+  } else {
+    console.warn('Push messaging is not supported');
+    pushButton.textContent = 'Push Not Supported';
+  }
+}
 
 function initRouter() {
   const router = new Router(document.querySelector('main'));
