186cc1ba52d42c8801b8c061126d789e3a5c5ae0
Greta Žebrauskaitė <greta@halopowered.com>
2019-11-28 11:44:13 +0200

    add shaky shaky navigation

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,16 +1,8 @@
 export const SELECT_BOARD = 'SELECT_BOARD';
-export const SET_VIEW = 'SET_VIEW';
 
 export const selectRoom = board => {
   return {
     type: SELECT_BOARD,
     board
-  };
-};
-
-export const setView = view => {
-  return {
-    type: SET_VIEW,
-    view
   };
 };
