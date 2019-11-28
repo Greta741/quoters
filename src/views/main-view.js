09e6983e20b5c617d7564d591dde0b33e324c837
Paulius Rimgaila <paulius@halopowered.com>
2019-11-28 13:34:49 +0200

    Update slider

--- src/views/main-view.js
+++ src/views/main-view.js
@@ -1,12 +1,10 @@
-import { html } from 'lit-element';
-import {
-  getVisibleQuotes
-} from '../redux/reducer.js';
-import { connect } from 'pwa-helpers';
-import { store } from '../redux/store.js';
-import { BaseView } from './base-view.js';
-import '../components/my-quote.js';
-import { selectRoom } from '../redux/actions.js';
+import { html } from "lit-element";
+import { getVisibleQuotes } from "../redux/reducer.js";
+import { connect } from "pwa-helpers";
+import { store } from "../redux/store.js";
+import { BaseView } from "./base-view.js";
+import "../components/my-quote.js";
+import { selectRoom } from "../redux/actions.js";
 
 class MainView extends connect(store)(BaseView) {
   static get properties() {
@@ -21,9 +19,9 @@ class MainView extends connect(store)(BaseView) {
 
   updateQuotes() {
     if (this.location.params.room) {
-        store.dispatch(selectRoom(this.location.params.room));
+      store.dispatch(selectRoom(this.location.params.room));
     } else {
-        store.dispatch(selectRoom(null));
+      store.dispatch(selectRoom(null));
     }
   }
 
@@ -31,21 +29,141 @@ class MainView extends connect(store)(BaseView) {
     this.updateQuotes();
 
     return html`
+      <style>
+        button {
+          background-color: inherit;
+          color: #333;
+          border: none;
+          opacity: 0.3;
+        }
 
-    Room: ${this.location.params.room}
-    <ul>
-    ${
-        this.quotes.map(
-            quote => html`
-                <li>
-                    <my-quote quoteText="${quote.quote}"  qouteAuthor="${quote.author}"></my-quote>
-                </li>
-                `
-        )
-    }
-    </ul>
+        button:hover {
+          opacity: 1;
+        }
+
+        a {
+          text-decoration: none;
+          opacity: 0.3;
+        }
+
+        a:hover {
+          opacity: 1;
+        }
+
+        .container {
+          margin: 0 auto;
+        }
+
+        .panel-quote {
+          position: absolute;
+          top: 50%;
+          left: 50%;
+          transform: translate(-50%, -50%);
+          min-width: 400px;
+          margin: auto;
+          box-sizing: border-box;
+          background-color: #fff;
+        }
+
+        .social-share {
+          text-align: center;
+        }
+
+        .social-share i {
+          color: #333;
+        }
+
+        .quote-progress {
+          width: 0;
+          height: 3px;
+          background-color: #333;
+        }
+
+        blockquote {
+          padding: 30px;
+          font-size: 1.4em;
+        }
+
+        .quote {
+          font-family: "Crimson Text", serif;
+          font-style: italic;
+        }
+
+        .author {
+          font-size: 0.6em;
+          font-weight: lighter;
+          text-align: right;
+        }
+
+        /* Quote Navigation */
+        .quote-nav {
+          display: flex;
+          align-items: stretch;
+          width: 100%;
+          padding-bottom: 30px;
+        }
+
+        .previous {
+          margin: auto;
+        }
+
+        .next {
+          margin: auto;
+        }
+
+        /* Media Queries */
+        @media screen and (max-width: 460px) {
+          .panel-quote {
+            min-width: 100%;
+          }
+
+          blockquote {
+            font-size: 1.1em;
+          }
+        }
+      </style>
+
+      <div class="container">
+        <div class="panel-quote">
+          <div class="quote-progress"></div>
+          <div>
+            ${this.quotes.map(
+              quote => html`
+                <blockquote>
+                  <div class="social-share">
+                    <a
+                      href="#"
+                      id="quote-tweet"
+                      title="Tweet this quote"
+                      target="_blank"
+                    >
+                      <i class="fa fa-twitter" aria-hidden="true"></i>
+                    </a>
+                  </div>
+                  <p class="quote">${quote.quote}</p>
+                  <p class="author">
+                    - ${quote.author}<span class="author-name"></span>
+                  </p>
+                </blockquote>
+                <div class="quote-nav">
+                  <button class="previous">
+                    <i class="fa fa-long-arrow-left" aria-hidden="true"></i>
+                  </button>
+                  <button class="random">
+                    <i class="fa fa-random" aria-hidden="true"></i>
+                  </button>
+                  <button class="next">
+                    <i class="fa fa-long-arrow-right" aria-hidden="true"></i>
+                  </button>
+                </div>
+              `
+            )}
+          </div>
+        </div>
+      </div>
+      <ul></ul>
     `;
   }
 }
 
-customElements.define('main-view', MainView);
+customElements.define("main-view", MainView);
